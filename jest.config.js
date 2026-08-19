15c200f6de29c9f2a21adb91bcd2f33efd5dec4d
andrea110292-droid <andrea.110292@gmail.com>
2026-08-19 11:34:08 -0500

    chore: elevar umbral de cobertura de 70% a 80%
    La cobertura actual (92% statements, 83% functions) permite un umbral
    mas exigente que el minimo sugerido, manteniendo margen ante regresiones.

--- jest.config.js
+++ jest.config.js
@@ -36,6 +36,6 @@ module.exports = {
     '!src/mocks/**',
   ],
   coverageThreshold: {
-    global: { branches: 70, functions: 70, lines: 70, statements: 70 },
+    global: { branches: 80, functions: 80, lines: 80, statements: 80 },
   },
 };
